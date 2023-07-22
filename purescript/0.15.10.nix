caf9622ab01096756775eaef12a3ceeef0b7057f
amesgen <amesgen@amesgen.de>
2023-07-22 19:50:01 +0200

    Add purescript 0.15.10

---
+++ purescript/0.15.10.nix
@@ -0,0 +1,32 @@
+{ pkgs }:
+
+let
+  inherit (pkgs) system;
+  version = "0.15.10";
+
+  urls = {
+    "x86_64-linux" = {
+      url = "https://github.com/purescript/purescript/releases/download/v${version}/linux64.tar.gz";
+      hash = "sha256-OZC2xQXEKm6qDd9AkHyn2Zn/mmDOE81UfDjvXqpw5Q4=";
+    };
+    "x86_64-darwin" = {
+      url = "https://github.com/purescript/purescript/releases/download/v${version}/macos.tar.gz";
+      hash = "sha256-7zfdJ7lFrrMZE825pOsgKFx7FfC7EOOE5NbqNjYAzZM=";
+    };
+  };
+
+  src =
+    if builtins.hasAttr system urls then
+      (pkgs.fetchurl urls.${system})
+    else if system == "aarch64-darwin" then
+      let
+        useArch = "x86_64-darwin";
+        msg = "Using the non-native ${useArch} binary. While this binary may run under Rosetta 2 translation, no guarantees can be made about stability or performance.";
+      in
+      pkgs.lib.warn msg (pkgs.fetchurl urls.${useArch})
+    else
+      throw "Architecture not supported: ${system}";
+in
+import ./mkPursDerivation.nix {
+  inherit pkgs version src;
+}
