2e38b847a38911b0d99b3aac03b088927d0ba25a
Peter Becich <peterbecich@gmail.com>
2024-01-05 13:53:10 -0800

    add PureScript 0.15.14
    SHAs retrieved from https://github.com/purescript/purescript/releases/tag/v0.15.14

---
+++ purescript/0.15.14.nix
@@ -0,0 +1,32 @@
+{ pkgs }:
+
+let
+  inherit (pkgs) system;
+  version = "0.15.14";
+
+  urls = {
+    "x86_64-linux" = {
+      url = "https://github.com/purescript/purescript/releases/download/v${version}/linux64.tar.gz";
+      hash = "sha256:393a8955b40e945095f469a976421f8dfa595040";
+    };
+    "x86_64-darwin" = {
+      url = "https://github.com/purescript/purescript/releases/download/v${version}/macos.tar.gz";
+      hash = "sha256:6238f3691d74c0f0a636a562b055f04ea0a7680f";
+    };
+  };
+
+  src =
+    if builtins.hasAttr system urls then
+      (pkgs.fetchurl urls.${system})
+    else if system == "aarch64-darwin" then
+      let
+        useArch = "x86_64-darwin";
+        msg = "Using the non-native ${useArch} binary. While this binary may run under Rosetta 2 translation, no guarantees can be made about stability or performance.";
+      in
+      pkgs.lib.warn msg (pkgs.fetchurl urls.${useArch})
+    else
+      throw "Architecture not supported: ${system}";
+in
+import ./mkPursDerivation.nix {
+  inherit pkgs version src;
+}
